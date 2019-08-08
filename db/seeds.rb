5e9786e1d6fe6f9b6a2607e59586ff3fc468d7e2
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-08 11:48:08 +0900

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = JSON.parse(open(url).read)
+
+drinks['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
