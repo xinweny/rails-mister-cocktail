931941d1df2e7cbefcf06b62be5b32f87b3ff4d8
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-09 14:07:10 +0900

    cleaned db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
 require 'json'
 
-Ingredient.destroy_all!
+Ingredient.destroy_all
+Cocktail.destroy_all
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 drinks = JSON.parse(open(url).read)
